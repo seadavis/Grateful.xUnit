7c85cb31ccb0d6660ca783864d183c81bcef46d6
Sean Davis <seadav.17@gmail.com>
2022-05-08 20:37:33 -0600

    Adding Test Projects

--- src/xAPI/Class1.cs
+++ src/xAPI/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace xAPI
+﻿using System.Reflection;
+using Xunit.Sdk;
+
+namespace xAPI
 {
-   public class Class1
+   public class RequireASPNet : BeforeAfterTestAttribute
    {
+      public override void Before(MethodInfo methodUnderTest)
+      {
+
+      }
+
+      public override void After(MethodInfo methodUnderTest)
+      {
+
+      }
 
    }
 }
